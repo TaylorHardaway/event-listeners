d63490e262c4edc235c80cc463481d9acc3894a0
TaylorHardaway <gthardaway@gmail.com>
2021-01-23 11:08:48 -0600

    notes on callbacks

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("Am I Connected?");
+
+function saysHelloToUser (user) {
+  return 'Hello ${user}!';
+}
+
+function saysGoodbyeToUser(user) {
+  return 'Goodbye ${user}!';
+}
+
+function createGreeting(user, cb) {
+  return cb(user);
+}
+
+console.log(createGreeting('Dan', saysHelloToUser)); // 'Hello Dan!
+console.log(createGreeting('Dan', saysGoodbyeToUser)); // 'Goodbye Dan!
